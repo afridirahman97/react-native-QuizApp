2f7efede426220c12e986f3f4c9c25f8ddf79943
unknown <afridirahman97@gmail.com>
2021-01-03 14:11:41 +0600

    final

---
+++ react-native-quizz/App.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import Home from "./screens/Home";
+import Quizz from "./screens/Quizz";
+import Results from "./screens/Results";
+import { RootStackParamList } from "./types/navigation";
+import { Provider } from "react-redux";
+import { createSharedElementStackNavigator } from "react-navigation-shared-element";
+import store from "./store";
+import { human } from "react-native-typography";
+import HeaderRight from "./components/HeaderRight";
+type AppProps = Record<string, unknown>;
+
+const Stack = createSharedElementStackNavigator<RootStackParamList>();
+const App: React.FunctionComponent<AppProps> = () => {
+  return (
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Home"
+          screenOptions={{
+            headerTitle: "HOME OF QUIZ",
+            headerTintColor: "white",
+            // eslint-disable-next-line react/display-name
+            headerRight: ({ tintColor }: { tintColor?: string }) => (
+              <HeaderRight
+                tintColor={tintColor}
+                toggleMusic={store.dispatch.settings.toggleMusic}
+              />
+            ),
+            headerTitleStyle: human.title2White,
+            headerStyle: {
+              backgroundColor: "#CB0B0B",
+              borderBottomWidth: 0,
+              shadowRadius: 0,
+              shadowOffset: {
+                height: 0,
+                width: 0,
+              },
+            },
+          }}
+        >
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen
+            name="Quizz"
+            component={Quizz}
+            sharedElementsConfig={(route) => {
+              const { category } = route.params;
+              return [category];
+            }}
+          />
+          <Stack.Screen
+            name="Results"
+            component={Results}
+            options={{
+              headerTitleStyle: human.title2,
+              headerStyle: { backgroundColor: "#ffc542" },
+              headerTintColor: "black",
+              headerLeft: () => null,
+            }}
+            sharedElementsConfig={() => {
+              return undefined;
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
+  );
+};
+
+export default App;
